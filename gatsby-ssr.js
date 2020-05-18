498fc33bc08c57289e447006de7f021baed9754e
Jamie Barton <jamie@notrab.dev>
2020-05-18 08:26:59 +0100

    use Token over Auth for provider name

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,17 @@
 import React from "react"
 import cuid from "cuid"
 
-import CustomerAuthProvider from "./src/components/CustomerTokenProvider"
+import CustomerTokenProvider from "./src/components/CustomerTokenProvider"
 import ApolloProvider from "./src/components/ApolloProvider"
 import CartProvider from "./src/components/CartProvider"
 import Layout from "./src/components/Layout"
 
 export const wrapRootElement = ({ element }) => (
-  <CustomerAuthProvider>
+  <CustomerTokenProvider>
     <ApolloProvider>
       <CartProvider id={cuid()}>
         <Layout>{element}</Layout>
       </CartProvider>
     </ApolloProvider>
-  </CustomerAuthProvider>
+  </CustomerTokenProvider>
 )
