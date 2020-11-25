e03ecf57eb6a134e6b8359138b223103194e448e
Jamie Barton <jamie@notrab.dev>
2020-11-25 23:32:41 +0000

    add basic thankyou page

---
+++ src/pages/thankyou.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ThankyouPage = () => {
+  return (
+    <React.Fragment>
+      <h1>Thank you</h1>
+
+      <p>Thanks for your order.</p>
+    </React.Fragment>
+  )
+}
+
+export default ThankyouPage
